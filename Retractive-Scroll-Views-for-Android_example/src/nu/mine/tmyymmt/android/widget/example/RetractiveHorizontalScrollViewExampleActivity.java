934d79626e3a79dc70695508c1e664f93013fa2d
tmyymmt+github@gmail.com <tomoya@mail.com>
2012-03-31 15:14:03 -0700

    add example.

---
+++ Retractive-Scroll-Views-for-Android_example/src/nu/mine/tmyymmt/android/widget/example/RetractiveHorizontalScrollViewExampleActivity.java
@@ -0,0 +1,13 @@
+package nu.mine.tmyymmt.android.widget.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RetractiveHorizontalScrollViewExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
